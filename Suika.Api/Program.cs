9b271ef489032fe130d7b1e6a2f2f0ef474ebf22
oNinjaZ <ninjaz.gg@outlook.com>
2022-05-03 16:51:14 -0400

    add database initializer

--- Suika.Api/Program.cs
+++ Suika.Api/Program.cs
@@ -1,12 +1,19 @@
+using Suika.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IDbConnectionFactory>(_ => new SqliteConnectionFactory(
+    builder.Configuration.GetValue<string>("Database:ConnectionString")
+));
+builder.Services.AddSingleton<DatabaseInitializer>();
+
+
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -14,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
+await databaseInitializer.InitializeAsync();
 
 app.Run();
