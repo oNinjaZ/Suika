453c37009451455d96256150174401d91165d0d0
oNinjaZ <marcbelzamour@yahoo.com>
2022-05-03 22:02:39 -0400

    updated validation rules

--- Suika.Api/Validators/UserValidator.cs
+++ Suika.Api/Validators/UserValidator.cs
@@ -7,8 +7,9 @@ public class UserValidator : AbstractValidator<User>
 {
     public UserValidator()
     {
-        RuleFor(user => user.Username).NotEmpty().WithMessage("Username field must not be empty.");
-        RuleFor(user => user.Username).Length(3, 20).WithMessage("");
+        RuleFor(user => user.Username).NotEmpty().WithMessage("Must not be empty.");
+        RuleFor(user => user.Username).Length(3, 20).WithMessage("Must be between 3 and 20 characters long.");
+        RuleFor(user => user.Username).Matches("^[a-zA-Z0-9]*$").WithMessage("Must contain only letters and numbers.");
 
         RuleFor(user => user.Email).NotEmpty();
         // todo - email regex
