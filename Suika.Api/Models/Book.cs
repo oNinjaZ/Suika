9102245b919655a3f48454b976cc07a6b6f7fa87
oNinjaZ <marcbelzamour@yahoo.com>
2022-05-05 22:47:25 -0400

    Add Book model

--- Suika.Api/Models/Book.cs
+++ Suika.Api/Models/Book.cs
@@ -4,6 +4,6 @@ public class Book
 {
     public int BookId { get; set; }
     public string Title { get; set; } = default!;
+    public string Type { get; set; } = default!;
     public int PageCount { get; set; }
-    public DateTime ReleaseDate { get; set; }
 }
