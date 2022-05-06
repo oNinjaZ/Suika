f1c88b6f2ba98cc2fe3e73acfbd30a3bbd2adc28
oNinjaZ <marcbelzamour@yahoo.com>
2022-05-05 22:47:09 -0400

    Add Books Table

--- Suika.Api/Data/DatabaseInitializer.cs
+++ Suika.Api/Data/DatabaseInitializer.cs
@@ -16,22 +16,21 @@ public class DatabaseInitializer
         var connection = await _connectionFactory.CreateConnectionAsync();
         await connection.ExecuteAsync(@"
             CREATE TABLE IF NOT EXISTS Users(
-                UserId INTEGER PRIMARY KEY NOT NULL,
+                UserId INTEGER PRIMARY KEY,
                 Username TEXT NOT NULL,
                 Email TEXT,
                 RegistrationDate TEXT NOT NULL);
-            
-            CREATE TABLE IF NOT EXISTS Books(
-                BookId INTEGER NOT NULL PRIMARY KEY,
-                Title TEXT NOT NULL,
-                PageCount INTEGER,
-                ReleaseDate TEXT);
-            
-            CREATE TABLE IF NOT EXISTS UserBooks(
-                UserBooksId INTEGER NOT NULL PRIMARY KEY,
-                Status INTEGER NOT NULL,
-                UserId INTEGER NOT NULL,
-                FOREIGN KEY (UserId) REFERENCES Users(UserId));");
 
+            CREATE TABLE IF NOT EXISTS Books(
+                BookId INTEGER PRIMARY KEY,
+                Title TEXT NOT NULL,
+                Type TEXT NOT NULL,
+                PageCount INTEGER);");
+
+                // CREATE TABLE IF NOT EXISTS UserBooks(
+                // UserBooksId INTEGER NOT NULL PRIMARY KEY,
+                // Status INTEGER NOT NULL,
+                // UserId INTEGER NOT NULL,
+                // FOREIGN KEY (UserId) REFERENCES Users(UserId));
     }
 }
