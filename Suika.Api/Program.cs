321e4738c5db850ca07da0515d8ff8546e1948ca
oNinjaZ <marcbelzamour@yahoo.com>
2022-05-12 09:23:34 -0400

    update UserBook Post route

--- Suika.Api/Program.cs
+++ Suika.Api/Program.cs
@@ -125,8 +125,12 @@ app.MapGet("/books", async (IBookService bookService) =>
 #endregion
 
 #region UserBooks endpoints 
-app.MapPost("/{username}/books", async (string username, UserBook userBook, IUserBookService userBookService) =>
+app.MapPost("/{username}/books", async (string username, UserBook userBook, IUserService userService, IUserBookService userBookService) =>
 {
+    var existingUser = await userService.GetByUsernameAsync(username);
+    if(existingUser is null) return Results.BadRequest("User not found");
+
+    userBook.UserId = existingUser.UserId;
     var created = await userBookService.CreateAsync(userBook);
     if (!created) return Results.BadRequest();
     return Results.Ok();
