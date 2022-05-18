a8ec960588db562fbbbbfbcc6a9acf1998dd683e
oNinjaZ <92533441+MarcAtlanticBio@users.noreply.github.com>
2022-05-18 10:16:16 -0400

    restructure

--- Suika.Api/Program.cs
+++ Suika.Api/Program.cs
@@ -10,30 +10,30 @@ using Suika.Api.Extensions;
 using Suika.Api.Models;
 using Suika.Api.Services;
 
-#region Services
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Configuration.AddJsonFile("appsettings.Local.json", true, true);
 
 builder.Services.AddAuthentication(ApiKeySchemeConstants.SchemeName)
     .AddScheme<ApiKeyAuthSchemeOptions, ApiKeyAuthHandler>(ApiKeySchemeConstants.SchemeName, _ => { });
 builder.Services.AddAuthorization();
 
+// add swagger
+builder.Services.AddEndpointsApiExplorer()
+    .AddSwaggerGen();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// add custom endpoints
+builder.Services.AddUserEndpoints()
+    .AddBookEndpoints()
+    .AddUserBookEndpoints();
 
-builder.Services.AddUserEndpoints();
-builder.Services.AddBookEndpoints();
-builder.Services.AddSingleton<IUserBookService, UserBookService>();
-
+// add validators
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
+// add DB and initializer
 builder.Services.AddSingleton<IDbConnectionFactory>(_ => new SqliteConnectionFactory(
     builder.Configuration.GetValue<string>("Database:ConnectionString")
 ));
 builder.Services.AddSingleton<DatabaseInitializer>();
-#endregion
+
 
 var app = builder.Build();
 
@@ -45,27 +45,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.UseUserEndpoints();
-app.UseBookEndpoints();
-
-#region UserBooks endpoints 
-app.MapPost("/{username}/books", async (string username, UserBook userBook, IUserService userService, IUserBookService userBookService) =>
-{
-    var existingUser = await userService.GetByUsernameAsync(username);
-    if (existingUser is null) return Results.BadRequest("User not found");
-
-    userBook.UserId = existingUser.UserId;
-    var created = await userBookService.CreateAsync(userBook);
-    if (!created) return Results.BadRequest();
-    return Results.Ok();
-});
-
-app.MapGet("/{username}/books", async (string username, IUserBookService userBookService) =>
-{
-    var userBooks = await userBookService.GetAllAsync(username);
-    return Results.Ok(userBooks);
-});
-#endregion
+app.UseUserEndpoints()
+    .UseBookEndpoints()
+    .UseUserBookEndpoints();
 
 var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
 await databaseInitializer.InitializeAsync();
